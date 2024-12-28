42c55a281eb99d11f81288bcf682adc64902777b
Echpochmakk <70603461+Echpochmakk@users.noreply.github.com>
2024-12-28 13:48:16 +0300

    Update makefile.cmd

--- makefile.cmd
+++ makefile.cmd
@@ -1,13 +1,2 @@
-all: main.exe
-
-main.exe: main.o
-	gcc -o main.exe main.o
-
-main.o: main.c
-	gcc -c main.c
-
-X.o: X.c
-	gcc -c X.c
-
-clean:
-	rm main.o main.exe
+all: 
+    gcc -o lab.exe main.c X.c 
